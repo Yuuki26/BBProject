c4a698ed1780def5e1e02a0b25630ae94eb3f16f
Sunz <131935900+0931130106@users.noreply.github.com>
2025-12-02 20:44:46 +0700

    Update Frames.java
    Signed-off-by: Sunz <131935900+0931130106@users.noreply.github.com>

--- BB Project/BB/src/com/bb/Frames.java
+++ BB Project/BB/src/com/bb/Frames.java
@@ -1,51 +1,55 @@
 package com.bb;
 
-import javax.swing.*;
-import java.awt.*;
+import Ships.DefaultFleet;
+import com.bb.GameLayout.Mode;
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import java.awt.Component;
+import java.awt.Dimension;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
 public class Frames extends JFrame {
-
     private final CardLayout cl = new CardLayout();
-    private final JPanel cards = new JPanel(cl);
+    private final JPanel cards;
 
     public Frames() {
         super("Battleship Cards Demo");
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-
-        initUI();
-
-        pack();
-        setLocationRelativeTo(null);
-        setMinimumSize(new Dimension(2560, 1280));
+        this.cards = new JPanel(this.cl);
+        this.setDefaultCloseOperation(3);
+        this.initUI();
+        this.pack();
+        this.setLocationRelativeTo((Component)null);
+        this.setMinimumSize(new Dimension(2560, 1280));
     }
 
     private JPanel wrapper(JPanel content) {
         JPanel wrapper = new JPanel(new BorderLayout());
-        wrapper.add(content, BorderLayout.CENTER);
-        wrapper.add(new Navigator(cl, cards), BorderLayout.SOUTH);
+        wrapper.add(content, "Center");
+        wrapper.add(new Navigator(this.cl, this.cards), "South");
         return wrapper;
     }
 
     private void initUI() {
-
-        GameLayout player   = new GameLayout(this);
-        OpponentPanel opponent=new OpponentPanel();
-
-
-        Skill_Dialogs selection = new Skill_Dialogs(cl, cards);
-        MainMenuPanel mainMenu  = new MainMenuPanel(cl, cards);
-
-        cards.add(mainMenu,          "MAIN_MENU");
-        cards.add(selection,         "Skills");
-        cards.add(wrapper(player),   "PLAYER");
-        cards.add(wrapper(opponent), "OPPONENT");
-
-        getContentPane().setLayout(new BorderLayout());
-        getContentPane().add(cards, BorderLayout.CENTER);
-
-        cl.show(cards, "MAIN_MENU");
+        GameLayout player = new GameLayout(this, Mode.PLAYER);
+        GameLayout opponent = new GameLayout(this, Mode.OPPONENT);
+        player.setEnemyTargetBoard(opponent);
+        opponent.setEnemyTargetBoard(player);
+        DefaultFleet templateFleet = new DefaultFleet();
+        opponent.randomizeFleet(templateFleet);
+        Skill_Dialogs selection = new Skill_Dialogs(this.cl, this.cards);
+        MainMenuPanel mainMenu = new MainMenuPanel(this.cl, this.cards);
+        this.cards.add(mainMenu, "MAIN_MENU");
+        this.cards.add(selection, "Skills");
+        this.cards.add(this.wrapper(player), "PLAYER");
+        this.cards.add(this.wrapper(opponent), "OPPONENT");
+        this.getContentPane().setLayout(new BorderLayout());
+        this.getContentPane().add(this.cards, "Center");
+        this.cl.show(this.cards, "MAIN_MENU");
     }
 
-
-
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> (new Frames()).setVisible(true));
+    }
 }
