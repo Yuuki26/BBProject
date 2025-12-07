2cbcd7801765b7ea4f3e223f5ff69aa029efa7ec
Yuuki26 <101171329+Yuuki26@users.noreply.github.com>
2025-12-07 21:18:15 +0700

    Update Rapid_Fire.java
    Signed-off-by: Yuuki26 <101171329+Yuuki26@users.noreply.github.com>

--- BB Project/BB/src/skills/Rapid_Fire.java
+++ BB Project/BB/src/skills/Rapid_Fire.java
@@ -10,13 +10,20 @@ public class Rapid_Fire implements Skills {
     public String getDescription() {
         return "Fire 2 salvos for the cost of 3 turns";
     }
-    @Override
-    public void activate(GameLayout ctx) {
 
-    }
     @Override
     public String getImage() {
         return "Rapid_Fire.png";
+    }
+    @Override
+    public float modify_DMG() {
+        return 1f;
+    }
+    public float  modify_shield () {
+        return 1f;
+    }
+    public float  modify_HP () {
+        return 1f;
     }
 
 }
